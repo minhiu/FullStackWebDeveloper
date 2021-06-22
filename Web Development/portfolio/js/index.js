9e00e76a2cab234e908fb640dcc3248288501b65
Hieu Pham <hieu.pham01@student.csulb.edu>
2021-06-22 23:59:02 +0700

    Update portfolio website

---
+++ Web Development/portfolio/js/index.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.top-container');
+ScrollReveal().reveal('.middle-container', { delay: 500 });
+ScrollReveal().reveal('.skills', { delay: 500 });
+ScrollReveal().reveal('.projects', { delay: 500 });
+ScrollReveal().reveal('.contact-me', { delay: 500 });
